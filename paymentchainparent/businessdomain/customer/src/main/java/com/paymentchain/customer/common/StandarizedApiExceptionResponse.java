7cee54b659b733fdec43ebfd7967dc4504d198f9
JAVIER FERNANDO ESTUPIÑAN ALBA <javierapyoscar@gmail.com>
2025-10-08 16:25:56 -0700

    despliegue exceptionsc customer

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/common/StandarizedApiExceptionResponse.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/common/StandarizedApiExceptionResponse.java
@@ -4,10 +4,45 @@
  */
 package com.paymentchain.customer.common;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  *
  * @author javie
  */
+
+
+@Schema(description="description")
+@NoArgsConstructor
+@Data
 public class StandarizedApiExceptionResponse {
+    @Schema(description = "description", name="type", requiredMode = Schema.RequiredMode.REQUIRED, example = "/errors/authentication/not-authorized" )
+    private String type;
+    
+    @Schema(description = "description", name="title", requiredMode = Schema.RequiredMode.REQUIRED, example = "The user does not have authotized" )
+    private String title;
+    
+    @Schema(description = "description", name="code", requiredMode = Schema.RequiredMode.NOT_REQUIRED, example = "192" )
+    private String code;
+    
+    @Schema(description = "description", name="detail", requiredMode = Schema.RequiredMode.NOT_REQUIRED, example = "The user does not have the propertly persmissions to acces the" 
+    + "resources, please contact with us https://sotobotero.com")
+    private String detail;
+    
+    @Schema(description = "description", name="instance", requiredMode = Schema.RequiredMode.NOT_REQUIRED, example = "/errors/authetication/not-authorized/01")
+    private String instance;
+    
+    public StandarizedApiExceptionResponse (String type, String title, String code, String detail){
+        super();
+        this.type = type;
+        this.title = title;
+        this.code = code;
+        this.detail = detail;
+        
+
+    }
+    
     
 }
---
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/exception/ApiExceptionHandler.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.paymentchain.customer.exception;
+
+import com.paymentchain.customer.common.StandarizedApiExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ *
+ * @author javie
+ */
+@RestControllerAdvice 
+public class ApiExceptionHandler {
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handlerUnknownHostException(Exception ex){
+        
+        StandarizedApiExceptionResponse standarizedApiExceptionResponse = new StandarizedApiExceptionResponse("TECNICO", "Input OutPut Error", "1024", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(standarizedApiExceptionResponse);
+    }
+    
+}
